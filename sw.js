9bd473dbdf614f70b0c69daf2fd03888210b1816
乙醇 <jiangyichun.jyc@alibaba-inc.com>
2026-08-18 17:24:54 +0800

    fix: 标题下拉点击外部区域自动关闭
    dd-overlay 缺少 .open 样式导致遮罩层始终隐藏，补 display:block 规则
    版本 202608181840

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'assetbook-202608181835';
+const VERSION = 'assetbook-202608181840';
 const ASSETS = ['./', './index.html', './styles.css', './core.js', './gist.js', './ui.js', './trades.js', './health.js',
                 './manifest.json', './icons/icon-192.png', './icons/icon-512.png',
                 './icons/apple-touch-icon.png', './icons/favicon.png'];
